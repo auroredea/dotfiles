d80047012e182e60c0ee8ebef1ea3387f0cae576
auroredea <auroredeamaral@gmail.com>
2016-03-29 22:39:54 +0200

    Add local symlink file after asking the user
    Issue: #

--- system/symlink_setup.sh
+++ system/symlink_setup.sh
@@ -28,3 +28,23 @@ ln -sf $SOLARIZED/dircolors-solarized/dircolors.ansi-light $HOME/.dircolors
 # vim solarized
 cd $HOME/.vim/bundle || exit
 ln -sf $SOLARIZED/vim-colors-solarized vim-colors-solarized
+
+# adding local files ?
+
+function localfiles {
+ cd $HOME
+ ln -s $DOTFILES/local/aliases.local $HOME/.aliases.local
+ ln -s $DOTFILES/local/gitconfig.local $HOME/.gitconfig.local
+ ln -s $DOTFILES/local/vimrc.local $HOME/.vimrc.local
+ ln -s $DOTFILES/local/zshrc.local $HOME/.zshrc.local
+ echo "local files installed"
+}
+
+while true; do
+    read -p "Do you wish to add the local files ? [y/n]" yn
+    case $yn in
+        [Yy]* ) localfiles; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
