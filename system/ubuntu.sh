36bd0764f7b5bc773aacbc2abfa8f4dea9ea12ae
Aurore de Amaral <auroredea@users.noreply.github.com>
2016-09-07 12:57:22 +0200

    Update ubuntu.sh error

--- system/ubuntu.sh
+++ system/ubuntu.sh
@@ -6,9 +6,6 @@
 
 export BIN=Applications
 
-# Ask for the administrator password upfront.
-sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
@@ -31,9 +28,6 @@ apt install zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL \
   https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Create a folder to hold all the solarized color schemes
-mkdir -p ${SOLARIZED:=$HOME/.solarized}
 
 # Adding diff-so-fancy for git log
 # See https://github.com/git/git/tree/master/contrib/diff-highlight
