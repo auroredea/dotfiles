a60a62dc126b004ccff631b877207ece2d6d9580
Aurore de Amaral <auroredea@users.noreply.github.com>
2017-05-07 19:51:34 +0200

    add vundle installation

--- system/symlink_setup.sh
+++ system/symlink_setup.sh
@@ -25,42 +25,28 @@ function localfiles {
  ln -s $DOTFILES/local/gitconfig.local $HOME/.gitconfig.local
  ln -s $DOTFILES/local/vimrc.local $HOME/.vimrc.local
  ln -s $DOTFILES/local/zshrc.local $HOME/.zshrc.local
- echo "local files installed"
+ echo "local files installed !"
 }
 
-# adding my vim plugins ?
+# adding vim files
 function vimplugins {
- cd $HOME/.vim/bundle
- #ctrlp add a hidden bar for searching for files
- git clone https://github.com/ctrlpvim/ctrlp.vim.git ctrlp.vim
- #emmet adds the power of emmet on VIM
- git clone https://github.com/mattn/emmet-vim.git emmet.vim
- #indentline adds a line showing the indentation level for source code  
- git clone https://github.com/Yggdroot/indentLine.git indentLine
- #lexima adds auto-closing all parentheses
- git clone https://github.com/cohama/lexima.vim.git lexima.vim
- #nerdcommenter adds comments on blocks and lines
- git clone https://github.com/scrooloose/nerdcommenter.git nerdcommenter
- #nerdtree adds a sweet tree companion for your projects
- git clone https://github.com/scrooloose/nerdtree.git nerdtree
- #surround add/change/delete all surrounding stuff (parantheses, tags,..)
- git clone https://github.com/tpope/vim-surround.git surround.vim
+  vim +PluginInstall +qall
 }
 
 while true; do
-    read -p "Do you wish to add the local files ? [y/n]" yn
+    read -p "Do you wish to add the local files ? [y/n] " yn
     case $yn in
         [Yy]* ) localfiles; break;;
         [Nn]* ) exit;;
-        * ) echo "Please answer [y]es or [n]o.";;
+        * ) echo "Please answer [y]es or [n]o ";;
     esac
 done
 
 while true; do
-    read -p "Do you wish to add selected vim plugins  ? [y/n]" yn
+    read -p "Do you wish to add selected vim plugins  ? [y/n] " yn
     case $yn in
         [Yy]* ) vimplugins; break;;
         [Nn]* ) exit;;
-        * ) echo "Please answer [y]es or [n]o.";;
+        * ) echo "Please answer [y]es or [n]o ";;
     esac
 done
