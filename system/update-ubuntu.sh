ba824e37f31c19d0daafb7d347afcce10fe1d103
Aurore de Amaral <2429626+auroredea@users.noreply.github.com>
2021-10-28 11:48:00 +0200

    create update for ubuntu

---
+++ system/update-ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+################################
+# TESTED WITH UBUNTU           #
+################################
+
+export BIN=applications
+
+echo "Mise à jour système"
+echo "Please use root password"
+sudo apt update
+
+echo "Mise à jour Dotfiles"
+cd $HOME/.dotfiles
+git checkout master && git pull --rebase --autostash
+
+echo "Mise à jour plugins vim"
+vim +PluginUpdate +qall
+
+echo "Mise à jour OH-MY-ZSH"
+cd $HOME/.oh-my-zsh
+git checkout master && git pull --rebase --autostash
